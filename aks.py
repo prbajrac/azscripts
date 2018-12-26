c4d132b2f9d1236bfb236e3fb4fbaa69d1ee6d6d
Prabuddha Bajracharya (PB) <prbajrac@microsoft.com>
2018-12-26 11:18:24 -0800

    Added Disclaimer

--- aks.py
+++ aks.py
@@ -14,7 +14,14 @@ cluster = raw_input ('Cluster Name: ')
  #print ('Script will resize the VM to ' + osDiskSize)
 #else:
  #print('Your OS Disk size will not be altered')
-
+print ('**************************************************************')
+print ("""Disclaimer The sample scripts are not supported under any Microsoft standard support program or service. The sample scripts are provided AS IS without warranty of any kind.
+Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose.
+The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. 
+In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever
+(including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or
+inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.""")
+print ('**************************************************************')
 
 ct = raw_input('Do you want to continue : ')
 ct = ct.upper()
@@ -66,11 +73,11 @@ if ct == 'Y':
   print ('**************************************************************')
  
   print(subprocess.check_output('az vm deallocate -g '+ nrg + ' -n ' + nodeName, shell=True))
-  print ('**************************************************************')
-  print ('UPDATING DISK SIZE')
-  print ('**************************************************************')
+  #print ('**************************************************************')
+  #print ('UPDATING DISK SIZE')
+  #print ('**************************************************************')
  
-  print(subprocess.check_output('az disk update -g ' + nrg + ' -n ' + osDiskName + ' --size-gb '+ osDiskSize + ' --sku \'Premium_LRS\'', shell=True))
+  #print(subprocess.check_output('az disk update -g ' + nrg + ' -n ' + osDiskName + ' --size-gb '+ osDiskSize + ' --sku \'Premium_LRS\'', shell=True))
   
   print ('**************************************************************')
   print ('STARTING VM')
